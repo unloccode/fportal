fbdadbcd4894533b0931f8a1b28a9d4614e8ce5f
unloccode <unloccode@gmail.com>
2021-07-20 23:02:05 +0300

    Few changes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,93 @@
 //import logo from './logo.svg';s
 import React from 'react';
+import { Switch, Route, Link } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 import './App.css';
 
+import AuthService from '../services/auth.service.js';
+
+import Login from '../components/login.component.js';
+import Profile from '../components/profile.component.js';
+
+
 class App extends React.Component{
+  constructor(props){
+    super(props);
+    this.logOut = this.logOut.bind(this);
+    this.state = {
+      showModeratorBoard: false,
+      showAdminBoard: false,
+      currentUser: undefined
+    };
+  }
+  componentDidMount(){
+    const user = AuthService.getCurrentUser();
+    if(user){
+      this.setState({
+        currentUser: user,
+        showModeratorBoard: user.roles.includes("ROLE_MODERATOR"),
+        showAdminBoard: user.roles.includes("ROLE_ADMIN"),
+      });
+    }
+  }
+  logOut(){
+    AuthService.logout();
+  }
   render(){
     //some awesome code
+    const { currentUser, showModeratorBoard, showAdminBoard } = this.state;
+    return(
+      <div>
+        <nav className="navbar navbar-expand navbar-dark bg-dark">
+          <Link to={"/"} className="navbar-brand">Unloccode</Link>
+          <div className="navba-nav mr-auto">
+            {showModeratorBoard && (
+              <li className="nav-item">
+                <Link to={"/mod"} className="nav-link">Moderator Board</Link>
+              </li>
+            )}
+            {showModeratorBoard && (
+              <li className="nav-item">
+                <Link to={"/admin"} className="nav-link">Admin Board</Link>
+              </li>
+            )}
+            {currentUser && (
+              <li className="nav-item">
+                <Link to={"/user"} className="nav-link">User</Link>
+              </li>
+            )}
+          </div>
+          {currentUser ? (
+            <div className="navbar-nav ml-auto">
+              <li className="nav-item">
+                <Link to={"/profile"} className="nav-link">
+                  {currentUser.username}
+                </Link>
+              </li>
+              <li className="nav-item">
+                <a href="/login" className="nav-link" onClick={this.logOut}>
+                  Logout
+                </a>
+              </li>
+            </div>
+          ): (
+            <div className="navbar-nav ml-auto">
+              <li className="nav-item">
+                <Link to={"/login"} className="nav-link">Login</Link>
+              </li>
+            </div>
+          )
+        }
+        </nav>
+        <div className="container mt-3">
+          <Switch>
+            <Route exact path="/login" component={Login} />
+            <Route exact path ="/profile" component={Profile} />
+          </Switch>
+        </div>
+      </div>
+    );
   }
 }
 
