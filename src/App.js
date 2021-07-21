75fdc0efa6d0c6efbd9c54593c14f3b3e6c3df61
unloccode <unloccode@gmail.com>
2021-07-21 22:46:36 +0300

    Added routes to App.js and updated user services

--- src/App.js
+++ src/App.js
@@ -11,6 +11,9 @@ import Login from './components/login.component';
 import Register from './components/register.component';
 import Home from './components/home.component';
 import Profile from './components/profile.component';
+import BoardUser from './components/board-user.component';
+import BoardModerator from './components/board-moderator.component';
+import BoardAdmin from './components/board-admin.component';
 
 
 class App extends React.Component{
@@ -95,6 +98,9 @@ class App extends React.Component{
             <Route exact path="/login" component={Login} />
             <Route exact path="/register" component={Register} />
             <Route exact path ="/profile" component={Profile} />
+            <Route path="/user" component={BoardUser} />
+            <Route path="/mod" component={BoardModerator} />
+            <Route path="/admin" component={BoardAdmin} />
           </Switch>
         </div>
       </div>
--- src/services/user.service.js
+++ src/services/user.service.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import authHeader from './auth.header.js';
 
-const API_URL = "http://localhost:8080/api/test";
+const API_URL = "http://localhost:8080/api/test/";
 
 class UserService{
     getPublicContent(){
